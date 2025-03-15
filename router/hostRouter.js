8a5ea4e83669b58e2e66db66518147d44c66d972
olliewiranphat <oil.wiranphat@gmail.com>
2025-03-15 23:21:47 +0700

    Update API Accomodation

--- router/hostRouter.js
+++ router/hostRouter.js
@@ -3,7 +3,7 @@ const authorHost = require('../middlewares/authorHost')
 const authenticaion = require('../middlewares/authenticaion')
 const { addImageCloud, delImageCloud } = require('../controllers/host/imagesController')
 const { getDashboard } = require('../controllers/host/hostController')
-const { addAccomTEST, deleteAccom, getAllMyAccom } = require('../controllers/host/accomController')
+const { deleteAccom, getAllMyAccom, addAccom, updateRoomImgs, updateAccomAmen } = require('../controllers/host/accomController')
 const hostRouter = express.Router()
 
 ///// Images Product by Cloudinary : 
@@ -11,11 +11,16 @@ hostRouter.post('/accommodation/add-image-cloud', authenticaion, authorHost, add
 hostRouter.post('/accommodation/delete-image-cloud', authenticaion, authorHost, delImageCloud) //Cloudinary Storage : delete Old Images
 
 ///// ADD Accom :
-hostRouter.post('/accommodation/add-new', addAccomTEST)
+hostRouter.post('/accommodation/add-new', authenticaion, authorHost, addAccom)
+
 ///// UPDATE Accom :
+hostRouter.put('/accommodation/room/update/:roomID', authenticaion, authorHost, updateRoomImgs) //Delete-Create New
+hostRouter.put('/accommodation/amenity/update/:accommodationID', authenticaion, authorHost, updateAccomAmen) //Delete-Create New
 hostRouter.put('/accommodation/update-data/:accommodationID', authenticaion, authorHost)
+
 ///// DELETE Accom :
 hostRouter.delete('/accommodation/delete/:accommodationID', authenticaion, authorHost, deleteAccom)
+
 ///// GET ALL My Accom:
 hostRouter.get('/accommodation/get-all', authenticaion, authorHost, getAllMyAccom)
 
