7d486bc9ed42d1daac58150f38b93ddb2834f86b
SasithornSP <S.pinpitak@gmail.com>
2025-03-19 13:33:07 +0700

    updateapi

--- middlewares/authorHost.js
+++ middlewares/authorHost.js
@@ -2,7 +2,7 @@ const createError = require("../utils/createError");
 const TryCatch = require("../utils/TryCatch");
 
 module.exports = TryCatch(async (req, res, next) => {
-    // console.log('req.user', req.user);
+    console.log('req.user', req.user);
     const { publicMetadata: { role } } = req.user
     role !== "HOST" && createError(401, "Access Denied Only HOST!")
 
